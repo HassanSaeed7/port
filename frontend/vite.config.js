3574e6ebc9721e6ab6114ca1a9d2d00acc81925e
Hassan <hsaeed1007@gmail.com>
2022-07-05 01:56:46 -0700

    predeploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,6 @@
-import path from 'path'
-
-export default {
-  build: {
-    outDir: path.resolve(__dirname, 'dist'),
-    rollupOptions: {
-      input: {
-        index: path.resolve(__dirname, 'index.html'),
-      }
+module.exports = {
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true 
     }
   }
-}
