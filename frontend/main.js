f4e47b6307c1582689ab460e21028289d8099d8d
Hassan <hsaeed1007@gmail.com>
2022-07-11 13:19:31 -0700

    changed navbar intersection observer logic

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,4 @@
-import "./src/output.css"
+import "./src/output.css";
 const targets = document.querySelectorAll("[data-src]");
 const navItems = document.querySelectorAll("[data-nav]");
 const home = document.querySelector("#home");
@@ -7,7 +7,7 @@ const spot = document.querySelector("#spot");
 const projects = document.querySelector("#projects");
 const contact = document.querySelector("#contact");
 const icons = document.querySelector("#icons");
-const sections = [spot, icons, projects, contact];
+const sections = [icons, projects, contact];
 const navSections = [home, about, projects, contact];
 const scrollToTop = document.querySelector("#nav");
 const logo = document.querySelector("#logo");
@@ -32,18 +32,41 @@ const lazyLoading = (target) => {
 targets.forEach(lazyLoading);
 
 //adds navbar after hero section
-const navObserver = new IntersectionObserver((entries, observer) => {
+// const navObserver = new IntersectionObserver((entries, observer) => {
+//   entries.forEach((entry) => {
+//     if (
+//       window.scrollY < 500 &&
+//       scrollToTop.classList.contains("flex" && "opacity-100")
+//     ) {
+//       scrollToTop.classList.remove("flex");
+//       scrollToTop.classList.remove("opacity-100");
+//       scrollToTop.classList.add("hidden");
+//       scrollToTop.classList.add("opacity-0");
+//     }
+//     if (entry.isIntersecting) {
+//       scrollToTop.classList.remove("hidden");
+//       scrollToTop.classList.add("flex");
+//       setTimeout(function () {
+//         scrollToTop.classList.remove("opacity-0");
+//         scrollToTop.classList.add("opacity-100");
+//       }, 50);
+//     }
+//   });
+// });
+
+// sections.forEach((section) => {
+//   navObserver.observe(section);
+// });
+
+// removes navbar when hero section is in view
+const heroObserver = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
-    if (
-      window.scrollY < 500 &&
-      scrollToTop.classList.contains("flex" && "opacity-100")
-    ) {
+    if (entry.isIntersecting) {
       scrollToTop.classList.remove("flex");
       scrollToTop.classList.remove("opacity-100");
       scrollToTop.classList.add("hidden");
       scrollToTop.classList.add("opacity-0");
-    }
-    if (entry.isIntersecting) {
+    } else {
       scrollToTop.classList.remove("hidden");
       scrollToTop.classList.add("flex");
       setTimeout(function () {
@@ -54,11 +77,7 @@ const navObserver = new IntersectionObserver((entries, observer) => {
   });
 });
 
-sections.forEach((section) => {
-  navObserver.observe(section);
-});
-
-
+heroObserver.observe(home);
 
 const navLi = document.querySelectorAll("[data-nav]");
 window.addEventListener("scroll", () => {
@@ -72,17 +91,16 @@ window.addEventListener("scroll", () => {
   });
 
   navLi.forEach((li) => {
-    li.classList.remove("bg-indigo-800")
+    li.classList.remove("bg-indigo-800");
     li.classList.add("bg-gray-800");
     li.classList.remove("w-full");
     li.classList.add("w-11/12");
-    const href = li.getAttribute('href').substring(1);
-		if (href === current) {
+    const href = li.getAttribute("href").substring(1);
+    if (href === current) {
       li.classList.remove("bg-gray-800");
       li.classList.add("bg-indigo-800");
       li.classList.remove("w-11/12");
-      li.classList.add("w-full"); 
-      
-		}
+      li.classList.add("w-full");
+    }
   });
 });
